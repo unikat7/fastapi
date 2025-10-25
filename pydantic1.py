57980db1de80ba12942e6c8bdba24c87d997d403
unikat7 <tamangunika38@gmail.com>
2025-10-25 10:00:34 +0545

    field validator on only one data

--- pydantic1.py
+++ pydantic1.py
@@ -1,27 +1,66 @@
-from pydantic import BaseModel
-from typing import List,Dict
+from pydantic import BaseModel,EmailStr,AnyUrl,Field,field_validator
+from typing import List,Dict,Optional,Annotated
 
 class Patient(BaseModel):
-    name:str
-    age:int
-    weight:float
-    married:bool
-    allergies:List[str]
+    # name:str=Field(max_legth=20)
+    name:Annotated[str,Field(max_length=2,title='name of the patient',description='give name less than 50 character',examples=['Nitish','sita'])]
+    age:int=Field(gt=0,le=120)
+    linkedin_url:AnyUrl
+    email:EmailStr
+    weight:Annotated[float,Field(gt=0,strict=True)]
+    # weight:float=Field(gt=0)
+    # married:bool=False
+    married:Annotated[bool,Field(default=None,description='is the patient married ot not')]
+    allergies:Optional[List[str]]=None
     contact_details:Dict[str,str]
+
+    @field_validator('email')
+    @classmethod
+    def email_validator(cls,value):
+        valid_domains=['hdfc.com','iccici.com']
+        domain_name=value.split('@')[-1]
+        if domain_name not in valid_domains:
+            raise ValueError('not a valid domain')
+        return value
+
+    @field_validator('name')
+    @classmethod
+    def transform_name(cls,value):
+        return value.upper()
+
+    @field_validator('age',mode='after')
+    @classmethod
+    def validate_age(cls,value):
+        if 0<value<100:
+            return value
+        else:
+            raise ValueError("age less or greater")
+    
+
+    
+    
+
+        
+
 
 
 def insert_patient_data(patient:Patient):
     print(patient.name)
     print(patient.age)
     print(patient.contact_details)
+    print(patient.married)
+    print(patient.allergies)
+    print(patient.linkedin_url)
 
 
 
 patient_info={
-    'name':'unika',
-    'age':'300',
+    'name':'u',
+    'age':'102',
+    'email':'abc@hdfc.com',
     'weight':75.2,
     'married':True,
+    'linkedin_url':'https://www.linkedin.com/in/unika-tamang-1aa522290/',
     'allergies':['pollen','dust'],
     'contact_details':{
         'email':'abc@gmail.com',
@@ -34,3 +73,6 @@ patient1=Patient(**patient_info
 )
 
 insert_patient_data(patient1)
+
+
+
